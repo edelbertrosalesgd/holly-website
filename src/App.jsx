a9784ff2a036795368f5d8573d0c187da4a3902e
edelbertrosalesgd <edelbertrosalesgd@gmail.com>
2026-05-18 07:58:25 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,8 @@
 export default function App() {
+  return (
+    <div style={{ padding: "50px", fontFamily: "Arial" }}>
+      <h1>Holly Lepage Website</h1>
+      <p>Website is working successfully.</p>
+    </div>
+  );
+}
